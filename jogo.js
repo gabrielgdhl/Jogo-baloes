9295a29b9caded1e73734dcd02fe92267d1748cc
gabrielgdhl <gabrielroquesousa@gmail.com>
2021-04-08 16:02:37 -0400

    foi adicionado os baloes a tela

--- jogo.js
+++ jogo.js
@@ -15,7 +15,27 @@ function iniciaJogo(){
     if(nivel_jogo == 3){
         tempo_segundos = 30;
     }
-
     document.getElementById('temp').innerHTML = tempo_segundos;
 
+    let qtd_baloes_estourados = 0;
+    document.getElementById('qtd-baloes-estourados').innerHTML = qtd_baloes_estourados;
+
+    //qtd balões da tela
+    let qtd_baloes = 70;
+    document.getElementById('qtd-baloes').innerHTML = qtd_baloes;
+    make_baloes(qtd_baloes);//função que adiciona os balões na tela.
+
+
+}
+
+
+function make_baloes(qtdd_baloes){
+    for(let i=1; i<=qtdd_baloes; i++){
+        
+        let balao = document.createElement('img');//cria uma tag <img>
+        balao.src = 'imagens/balao_azul_pequeno.png';//usar o atributo src de uma tag img para adiconar uma tag com uma imagem definida
+        balao.style.margin = '20px 0 10px 20px';//usa o atributo style das tags html para definir os pixel.
+        document.getElementById("cenario").appendChild(balao);
+
+    }
 }
